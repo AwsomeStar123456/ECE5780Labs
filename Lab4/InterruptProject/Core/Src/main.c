497f5a6cfe791c298d81a234dfdd3b599a307497
Semrah Odobasic <semrahodobasic@icloud.com>
2024-02-21 20:40:03 -0700

    Completed Lab 4 UART

---
+++ Lab4/InterruptProject/Core/Src/main.c
@@ -0,0 +1,416 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <string.h>
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void GPIO_Init(void);
+void USART_Init(void);
+void USART_SingleChar(char charVal);
+void USART_StringChar(char *string);
+char USART_ReadChar(void);
+char USART_NoDelayReadChar(void);
+void USART_LEDControl(void);
+
+int USART3NewVal = 0;
+int USART3BufferVal = 0;
+char USART3InputStr[2];
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  HAL_Init();
+	GPIO_Init();
+	USART_Init();
+	
+
+  while (1)
+  {
+		if(USART3NewVal)
+		{
+				//RED
+				if(USART3InputStr[0] == 'r')
+				{
+					if(USART3InputStr[1] == '0')
+					{
+						HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET);
+						USART_StringChar("RED LED Disabled");
+					}
+					else if(USART3InputStr[1] == '1')
+					{
+						HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
+						USART_StringChar("RED LED Enabled");
+					}
+					else if(USART3InputStr[1] == '2')
+					{
+						HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6);
+						USART_StringChar("RED LED Toggled");
+					}
+					else USART_StringChar("Invalid input");
+				}
+				//ORANGE
+				else if(USART3InputStr[0] == 'o')
+				{
+					if(USART3InputStr[1] == '0')
+					{
+						HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET);
+						USART_StringChar("ORANGE LED Disabled");
+					}
+					else if(USART3InputStr[1] == '1')
+					{
+						HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
+						USART_StringChar("ORANGE LED Enabled");
+					}
+					else if(USART3InputStr[1] == '2')
+					{
+						HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8);
+						USART_StringChar("ORANGE LED Toggled");
+					}
+					else USART_StringChar("Invalid input");
+				}
+				//GREEN
+				else if(USART3InputStr[0] == 'g')
+				{
+					if(USART3InputStr[1] == '0')
+					{
+						HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
+						USART_StringChar("GREEN LED Disabled");
+					}
+					else if(USART3InputStr[1] == '1')
+					{
+						HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);
+						USART_StringChar("GREEN LED Enabled");
+					}
+					else if(USART3InputStr[1] == '2')
+					{
+						HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9);
+						USART_StringChar("GREEN LED Toggled");
+					}
+					else USART_StringChar("Invalid input");
+				}
+				//BLUE
+				else if(USART3InputStr[0] == 'b')
+				{
+					if(USART3InputStr[1] == '0')
+					{
+						HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET);
+						USART_StringChar("BLUE LED Disabled");
+					}
+					else if(USART3InputStr[1] == '1')
+					{
+						HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
+						USART_StringChar("BLUE LED Enabled");
+					}
+					else if(USART3InputStr[1] == '2')
+					{
+						HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7);
+						USART_StringChar("BLUE LED Toggled");
+					}
+					else USART_StringChar("Invalid input");
+				}
+				else
+				{
+					USART_StringChar("Invalid input");
+				}
+				
+				USART_StringChar(" Input User Command: ");
+				USART3NewVal = 0;
+			}
+		}
+}
+
+/**
+  * @brief  Handles the interrupts for the USART3 peripheral.
+  * @retval None
+  */
+void USART3_4_IRQHandler(void)
+{
+	if(USART3BufferVal == 0)
+	{
+		USART3InputStr[0] = USART_NoDelayReadChar();
+		USART3BufferVal++;
+	} else {
+		USART3InputStr[1] = USART_NoDelayReadChar();
+		USART3NewVal = 1;
+		USART3BufferVal = 0;
+	}
+}
+
+/**
+  * @brief  Initiates GPIO Pins
+  * @retval None
+  */
+void GPIO_Init(void)
+{
+	//Enable GPIOB and GPIOC Clock
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	
+	//Init the RX pin PB11
+	GPIO_InitTypeDef rxTypeDef = {GPIO_PIN_11, GPIO_MODE_AF_PP, GPIO_SPEED_FREQ_LOW, GPIO_NOPULL, GPIO_AF4_USART3};
+	HAL_GPIO_Init(GPIOB, &rxTypeDef);
+	
+	//Init the TX pin PB10
+	GPIO_InitTypeDef txTypeDef = {GPIO_PIN_10, GPIO_MODE_AF_PP, GPIO_SPEED_FREQ_LOW, GPIO_NOPULL, GPIO_AF4_USART3};
+	HAL_GPIO_Init(GPIOB, &txTypeDef);		
+	
+	//Enable LED Pins
+	GPIO_InitTypeDef ledTypeDef = {GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9,
+	GPIO_MODE_OUTPUT_PP, GPIO_SPEED_FREQ_LOW, GPIO_NOPULL};
+	HAL_GPIO_Init(GPIOC, &ledTypeDef);
+	
+	//Enable RED LED
+	//HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
+	
+	return;
+}
+
+/**
+  * @brief  Initiates the USART
+  * @retval None
+  */
+void USART_Init(void)
+{
+	//Enable the USART 3 CLK
+	//Baud Rate: 115200
+	//PB10 TX | PB11 RX
+	__HAL_RCC_USART3_CLK_ENABLE();
+	
+	//Set Baud Rate
+	USART3->BRR = HAL_RCC_GetHCLKFreq() / 115200;
+	
+	//Enable Tx and Rx
+	USART3->CR1 |= ((1 << 3) | (1 << 2));
+	
+	//Enable the USART3
+	USART3->CR1 |= (1 << 0);
+	
+	//Enable the recieve interrupt
+	USART3->CR1 |= (1 << 5);
+	
+	//Enable the recieve interrupt in NVIC
+  NVIC_EnableIRQ(USART3_4_IRQn);
+	
+	//Set the priority of the recieve interrupt in the NVIC
+  NVIC_SetPriority(USART3_4_IRQn, 0);
+	
+	//Request User Input
+	USART_StringChar(" Input User Command: ");
+	
+	return;
+}
+
+/**
+  * @brief  Method for sending a character through a UART connection.
+  * @retval None
+  */
+void USART_SingleChar(char charVal)
+{
+	//Make sure TX data register is empty before writing to it.
+	while(!(USART3->ISR & (1 << 7)));
+	
+	//Write data to send.
+	USART3->TDR = charVal;
+	
+	return;
+}
+
+/**
+  * @brief  Method for sending a string through a UART connection.
+  * @retval None
+  */
+void USART_StringChar(char *string)
+{
+	int i = 0;
+	
+	//While we havent hit a termination character.
+	while(string[i] != '\0')
+	{
+		USART_SingleChar(string[i]);
+		i++;
+	}
+	
+	return;
+}
+
+/**
+  * @brief  Waits for the read reg to not be empty and reads its value.
+  * @retval Value that was read from the USART.
+  */
+char USART_ReadChar(void)
+{
+	//Wait until the read register is not empty.
+	while(!(USART3->ISR & (1 << 5)));
+	
+	//Return value of return register.
+	return USART3->RDR;
+}
+
+/**
+  * @brief  Reads the UART read reg value.
+  * @retval Value that was read from the USART.
+  */
+char USART_NoDelayReadChar(void)
+{
+	//Return value of return register.
+	return USART3->RDR;
+}
+
+/**
+  * @brief  Controls the LEDs based on USART input. Puts error message if there is an error.
+  * @retval None
+  */
+void USART_LEDControl(void)
+{
+	//Wait and read for input
+	char input = USART_ReadChar();
+	
+	//Based on input do something
+	switch(input)
+	{
+		case 'r':
+			HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6);
+			break;
+		case 'o':
+			HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8);
+			break;
+		case 'g':
+			HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9);
+			break;
+		case 'b':
+			HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7);
+			break;
+		default:
+		{
+			char str[] = "Invalid Character Inputted!";
+      USART_StringChar(str);
+		}
+	}
+}
+
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
